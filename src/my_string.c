5fc649bf4153c3644724efa06f0d3299ffe35b9f
Kuuhaku_kazari <SP_YYY@outlook.com>
2025-05-24 14:28:07 +0800

    重命名字符串操作函数为以 'k' 开头，统一命名规范

--- src/my_string.c
+++ src/my_string.c
@@ -5,14 +5,14 @@
 #include "string.h"
 
 void
-my_strcpy(char* dest, const char* src)
+kstrcpy(char* dest, const char* src)
 {
     // 循环复制字符，直到遇到字符串结束符'\0'
     while((*dest++ = *src++));
 }
 
 void
-my_strcat(char* dest, const char* src)
+kstrcat(char* dest, const char* src)
 {
     // 找到目标字符串的末尾
     while(*dest) dest++;
@@ -22,7 +22,7 @@ my_strcat(char* dest, const char* src)
 }
 
 int
-my_strcmp(const char* str1, const char* str2)
+kstrcmp(const char* str1, const char* str2)
 {
     // 循环比较字符，直到遇到字符串结束符'\0'
     while(*str1 && (*str1 == *str2))
@@ -36,7 +36,7 @@ my_strcmp(const char* str1, const char* str2)
 }
 
 int
-my_strlen(const char* str)
+kstrlen(const char* str)
 {
     int len = 0;
 
