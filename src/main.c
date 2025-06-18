be8042b1b96f0343e15890e916e8b5f5a492f354
Kuuhaku_kazari <SP_YYY@outlook.com>
2025-06-18 11:58:08 +0800

    修改

--- src/main.c
+++ src/main.c
@@ -1,9 +1,6 @@
 
 // main.c
 
-#include "mygos.h"
-
-
 extern void uart_init();         // 初始化串口
 extern void plic_init();         // 初始化外部中断控制器
 extern void page_init();         // 初始化分页
@@ -12,6 +9,7 @@ extern void timer_init();        // 初始化定时器
 extern void create_user_tasks(); // 创建任务
 extern void sched_init();        // 初始化调度器、开始任务调度
 
+extern void uart_puts(char* s);  // 输出字符串到串口（逐字符发送）
 
 void
 start_kernel()
